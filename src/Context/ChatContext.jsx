9b39aaf226cefffefa7b5d5e333290a91c70e817
Comeback Agency <pavel.dubenchuk.02@gmail.com>
2024-10-25 01:17:16 +0300

    fix

--- src/Context/ChatContext.jsx
+++ src/Context/ChatContext.jsx
@@ -1,84 +1,113 @@
-import { Children, createContext, useContext,useEffect, useState,useRef } from "react";
-import { StreamChat } from "stream-chat";
-import { Chat, useChatContext } from "stream-chat-react";
-import { useSelector } from "react-redux";
-import { MidLoading } from "../components/M_used/Loading";
-import { useLocation, useNavigate } from "react-router-dom";
+import {
+	Children,
+	createContext,
+	useContext,
+	useEffect,
+	useState,
+	useRef,
+} from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, useChatContext } from 'stream-chat-react';
+import { useSelector } from 'react-redux';
+import { MidLoading } from '../components/M_used/Loading';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 import { ZIM } from 'zego-zim-web';
 
-
-
 export const ChatContext = createContext({});
 
-const ChatContextProvider = ({children}) => {
-    const {user} = useSelector
-    ((state)=>state.auth);
-    const [chatClient, setChatClient] = useState();
-    const initChatCalled = useRef(false); // Ref to track if initChat has been called
-    const [unread,setUnread] = useState();
-    // const [zp,setZP] = useState(null);
-    let zp;
+const ChatContextProvider = ({ children }) => {
+	const { user } = useSelector(state => state.auth);
+	const [chatClient, setChatClient] = useState();
+	const initChatCalled = useRef(false); // Ref to track if initChat has been called
+	const [unread, setUnread] = useState();
+	// const [zp,setZP] = useState(null);
+	let zp;
 
-    console.log(user._id)
+	console.log(user._id);
 
-    const initChat = async() => {
-        if(!user || initChatCalled.current){
-            return ;
-        }
-        initChatCalled.current = true;
-        const client = StreamChat.getInstance("hxd9x3ag7hx3");
+	const initChat = async () => {
+		console.log('chatClient', chatClient);
 
-        await client.connectUser({
-            id: user._id,
-            name: user.username,
-            image: user.image
-        },user.stream_token);
+		if (!user || initChatCalled.current) {
+			return;
+		}
 
-        setUnread(client.user.total_unread_count);
+		initChatCalled.current = true;
 
-        setChatClient(client);
+		try {
+			if (chatClient && chatClient.userID === user._id) {
+				console.log('User is already connected to chat. No need to reconnect.');
+				return;
+			}
 
-        return () => {
-            setChatClient(null); // ADD THIS LINE!
-            chatClient.disconnectUser(); 
-          }
-    };
+			if (chatClient) {
+				await chatClient.disconnectUser();
+				setChatClient(null);
+				console.log('logout success', chatClient);
+			}
 
-    useEffect(() => {
-        if(user){
-        initChat();
-        }
-    },[user])
+			const client = StreamChat.getInstance('hxd9x3ag7hx3');
 
-    const startDMChatRoom = async (chatUser) => {
-        const newChannel = chatClient.channel("messaging", {
-            members: [user._id, chatUser._id]
-        });
-        await newChannel.watch();
-    }
+			await client.connectUser(
+				{
+					id: user._id,
+					name: user.username,
+					image: user.image,
+				},
+				user.stream_token
+			);
 
-    const deleteChat = async (event) => {
-        await event.destroy();
-    }
+			setUnread(client.user.total_unread_count);
+			setChatClient(client);
+			console.log('User successfully connected to chat!', client.userID);
+		} catch (error) {
+			console.error('Error connecting user to chat:', error);
+		}
+	};
 
-    const value = {startDMChatRoom,setChatClient,unread, deleteChat,zp,chatClient}
+	useEffect(() => {
+		if (user && !chatClient) {
+			initChat();
+		}
+	}, [user, chatClient]);
 
+	useEffect(() => {
+		console.log('chatClient', chatClient);
+	}, [chatClient]);
 
-    if(!chatClient){
-        return(
-            <MidLoading />
-        )
-    }
+	const startDMChatRoom = async chatUser => {
+		const newChannel = chatClient.channel('messaging', {
+			members: [user._id, chatUser._id],
+		});
+		await newChannel.watch();
+	};
 
-    return(
-        <Chat client={chatClient} theme='str-chat__theme-dark'>
-            <ChatContext.Provider value={value}>
-                {children}
-            </ChatContext.Provider>
-        </Chat>
-    )
-}
+	const deleteChat = async event => {
+		await event.destroy();
+	};
+
+	const value = {
+		startDMChatRoom,
+		setChatClient,
+		unread,
+		deleteChat,
+		zp,
+		chatClient,
+	};
+
+	return (
+		<>
+			{chatClient ? (
+				<Chat client={chatClient} theme='str-chat__theme-dark'>
+					<ChatContext.Provider value={value}>{children}</ChatContext.Provider>
+				</Chat>
+			) : (
+				<MidLoading />
+			)}
+		</>
+	);
+};
 
 export const useCustomChatContext = () => useContext(ChatContext);
 
--- src/Context/ProtectedRoute.jsx
+++ src/Context/ProtectedRoute.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { useSelector } from "react-redux";
-import { Navigate, useLocation } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
 import ChatContextProvider from './ChatContext';
-const ProtectedRoute = ({children}) => {
-    const {isAuthenticated} = useSelector((state) => state.auth);
+const ProtectedRoute = ({ children }) => {
+	const { isAuthenticated } = useSelector(state => state.auth);
 
-    let location = useLocation();
-    if(!isAuthenticated) {
-        return <Navigate to="/login" state={{ from: location}} replace />
-    }
- return (
-    <ChatContextProvider>
-        {children}
-    </ChatContextProvider>
- )
-
+	let location = useLocation();
+	if (!isAuthenticated) {
+		return <Navigate to='/login' state={{ from: location }} replace />;
+	}
+	return (
+		// <ChatContextProvider>
+		<>{children}</>
+		// </ChatContextProvider>
+	);
 };
 
 export default ProtectedRoute;
